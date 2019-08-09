a15608aab101fa1e6f27d4b51195b0d2bc18589d
James Parker <parkerpatriot@gmail.com>
2019-08-09 16:48:51 -0400

    init commit

---
+++ weightlifting.m
@@ -0,0 +1,34 @@
+clear
+close all
+
+data = csvread('weightlifting.csv')
+
+sex                         = data(:,1);  % sex
+age                         = data(:,2);  % age
+
+firstSnatchAttempt          = data(:,3);  % declared snatch weight
+firstSuccessSnatch          = data(:,4);  % first successful snatch weight
+maxSnatch                   = data(:,5);  % max snatch weight
+firstAttemptSnatchDelta     = data(:,6);  % delta between max and declared
+firstSuccessSnatchDelta     = data(:,7);  % delta between first success and max
+
+% firstClearnjerkAttempt      = data(:,8);  % declared clean-jerk weight
+% firstSuccessCleanjerk       = data(:,9);  % first successful clean-jerk weight
+% maxCleanjerk                = data(:,10); % max clean-jerk weight
+% firstAttemptCleanjerkDelta  = data(:,11); % delta between max and declared
+% firstSuccessCleanjerkDelta  = data(:,12); % delta between first success and max
+total                       = data(:,13); % total max weight "score"
+
+dm = [sex, age, firstSnatchAttempt, firstSuccessSnatch, maxSnatch, firstAttemptSnatchDelta, firstSuccessSnatchDelta, total];
+
+% plotmatrix(data)
+labels = {'gender', 'age', 'bweight', 'firstSnatchAttempt', 'firstSuccessSnatch', 'maxSnatch', 'firstAttemptSnatchDelta', 'firstSuccessSnatchDelta', 'total'};
+
+% labels = {'gender', 'age', 'bweight', 'firstSnatchAttempt', 'firstSuccessSnatch', 'maxSnatch', 'firstAttemptSnatchDelta', 'firstSuccessSnatchDelta', 'firstClearnjerkAttempt', 'firstSuccessCleanjerk', 'maxCleanjerk', 'firstAttemptCleanjerkDelta', 'firstSuccessCleanjerkDelta', 'total'};
+% data = [Height Weight Systolic Diastolic];
+
+[h,ax] = plotmatrix(data);
+for i = 1:length(data)
+  xlabel(ax(1,i), labels{i})
+  ylabel(ax(i,1), labels{i})
+end
