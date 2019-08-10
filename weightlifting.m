bb794c3ef1e1747fa36867db9bb27c20bea97421
James Parker <parkerpatriot@gmail.com>
2019-08-10 14:39:17 -0400

    incremental update

--- weightlifting.m
+++ weightlifting.m
@@ -22,34 +22,24 @@ cj_max2declared             = data(:,12); % delta between max and declared
 cj_max2firstSuccess         = data(:,13); % delta between first success and max
 total                       = data(:,14); % total max weight "score"
 
-dm = [age, bweight, snatch_max./cj_max, snatch_firstAttempt, snatch_firstSuccess, snatch_max, snatch_max2declared, cj_max, snatch_max2firstSuccess, total];
+dm = [age, bweight, snatch_max./cj_max, cj_max2declared + snatch_max2declared, total];
 
 labels = {...
     'age',...
     'bweight',...
     'snatch_{max}/cj_{max}',...
-    'snatch_{1stAttmp}',...
-    'snatch_{1stSucc}',...
-    'snatch_{max}',...
-    'snatch_{max-decl}',...
-    'cj_{max}',...
-    'snatch_{max-1stSucc}',...
+    'deltas_{total}',...
     'total'...
     };
 
-% labels = {'gender', 'age', 'bweight', 'snatch_firstAttempt', 'snatch_firstSuccess', 'snatch_max', 'snatch_max2declared', 'snatch_max2firstSuccess', 'cj_firstAttempt', 'cj_firstSuccess', 'cj_max', 'cj_max2declared', 'cj_max2firstSuccess', 'total'};
-
-[h,ax] = plotmatrix(dm);
+[~,ax] = plotmatrix(dm);
 for i = 1:length(labels)
   title(ax(1,i), labels{i})
   yl = ylabel(ax(i,1),labels{i},'fontweight','bold','Rotation', 0);
   yl.Position(1) = yl.Position(1) - 3.0;
-%   yl.Position(2) = yl.Position(2) - 0.6;
 end
 
-mdl1=fitlm([age, bweight, cj_max2declared],total,'VarNames',{'age' 'bweight','cj_max2declared','total'}) 
-mdl1=fitlm([age, bweight, snatch_max2declared],total,'VarNames',{'age' 'bweight','snatch_max2declared','total'}) 
-mdl1=fitlm([age, bweight, snatch_max./cj_max],total,'VarNames',{'age' 'bweight','snatch_max/cj_max','total'}) 
+mdl1 = fitlm([age, bweight, snatch_max./cj_max],total,'VarNames',{'age' 'bweight','snatch_max/cj_max','total'}) 
 
 r = mdl1.Residuals.Raw;
 figure
@@ -60,4 +50,11 @@ figure
 plot(mdl1)
 
 yhat = mdl1.Fitted;
+figure
+[~,ax] = plotmatrix([age, bweight, snatch_max./cj_max],r);
+labels = ['age','weight','ratio'];
+for i = 1:length(labels)
+  title(ax(1,i), labels{i})
+end
+figure
 plotmatrix(yhat,r)
