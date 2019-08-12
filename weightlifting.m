1d5e406e6e0ac7878e75c411249e102df024a537
James Parker <parkerpatriot@gmail.com>
2019-08-12 14:10:35 -0400

    Added live script version and generated pdf

--- weightlifting.m
+++ weightlifting.m
@@ -1,8 +1,9 @@
 clear
 close all
 
+%% Variables and Data
 
-% data from men's 73 kg weight class
+% import data from men's 73 kg weight class
 data = csvread('class73.csv');
 
 sex                         = data(:,1);  % sex
@@ -20,9 +21,14 @@ cj_max2declared             = data(:,12); % delta between max and declared
 cj_max2firstSuccess         = data(:,13); % delta between first success and max
 total                       = data(:,14); % total max weight "score"
 
-% metrics of interest
+%% Derived Metrics
+
+% Snatch to CJ ratio
 snatch_ratio = snatch_max./cj_max;
+% Sum of deltas between declared and max across both events
 total_deltas = cj_max2declared + snatch_max2declared;
+
+%% Initial Model - age, body weight, snatch ratio, deltas
 
 dm = [age, bweight, snatch_ratio, total_deltas, total];
 
@@ -34,6 +40,7 @@ labels = {...
     'total'...
     };
 
+% Create the plot matrix
 [~,ax] = plotmatrix(dm);
 for i = 1:length(labels)
   title(ax(1,i), labels{i})
@@ -43,35 +50,64 @@ end
 
 mdl1 = fitlm([age, bweight, snatch_ratio, total_deltas],total,'VarNames',{'age' 'bweight','snatch_max/cj_max','total_deltas','total'}) 
 
-r = mdl1.Residuals.Raw;
-figure
-histogram(r)
-title('Histogram of Residuals')
-figure
-normplot(r)
-title('Normplot of Residuals')
-figure
-plot(mdl1)
-title('Model Plot')
+%% Initial Findings:
+% Age and total deltas are not statistically significant.
+% Resvised model will exclude these two variables
 
+%% Auxiliary Plots
+
+% Generate CJ max and snatch max histograms
 figure
 subplot(1,2,1)
 histogram(cj_max,20)
 title('CJ Max')
+xlabel('Weight (kg)')
 subplot(1,2,2)
 histogram(snatch_max,20)
 title('Snatch Max')
+xlabel('Weight (kg)')
 
-yhat = mdl1.Fitted;
+%% Revised Model - body weight, snatch ratio
+
+mdl2 = fitlm([bweight, snatch_ratio],total,'VarNames',{'bweight','snatch_max/cj_max','total'}) 
+
+% Generate the confidence interterval for the model
+coefCI(mdl2)
+
+%% Revised Model: Residuals
+
+% Generate plot raw residuals 
+r2 = mdl2.Residuals.Raw;
 figure
-[~,ax] = plotmatrix([age, bweight, snatch_max./cj_max],r);
-titles = {'age' 'weight' 'ratio'};
+histogram(r2,20)
+title('Histogram of Residuals')
+
+% Generate residual plot for Xs 
+figure
+yhat = mdl2.Fitted;
+[~,ax] = plotmatrix([bweight, snatch_max./cj_max],r2);
+titles = {'Body Weight' 'Snatch Ratio'};
 for i = 1:length(titles)
     title(ax(i), titles(i))
     ylabel(ax(i),'Residuals')
 end
 
+% Generate residual plot for output
 figure
-plotmatrix(yhat,r)
+plotmatrix(yhat,r2)
+title('Residual Plot (y)')
 ylabel('Residuals')
 xlabel('$$\hat{y}$$', 'Interpreter', 'Latex');
+
+% Generate norm plot of residuals for analyzing
+% (Helpful for identifying heavy-tail vs light-tail distributions)
+figure
+normplot(r2)
+title('Normplot of Residuals')
+figure
+
+%% Revised Model: Plot
+
+% Generate model plot (fitted line)
+plot(mdl2)
+title('Model Plot')
